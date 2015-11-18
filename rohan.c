1acf3ffb52610cc75d5a7e8df0ff6387c70883d4
shwang90 <stevenhwang2013@gmail.com>
2015-11-17 20:47:12 -0500

    Added comments to the code and refactored function names

--- rohan.c
+++ rohan.c
@@ -19,7 +19,7 @@ int main() {
 
 	while (!found) {
 
-		if( is_perfect_sqrt(b,c,d) && is_cubert(a,b,c,d,e) ) {
+		if( is_perfect_sqr(b,c,d) && is_cubepow(a,b,c,d,e) ) {
 			found = 1;
 			printf("a = %i, b = %i, c = %i, d = %i, e = %i\n", a,b,c,d,e);
 		} else {
@@ -34,8 +34,20 @@ int main() {
 	return 0;
 }
 
-
-int is_cubert(int a, int b, int c, int d, int e) {
+/*
+* is_cubepow() returns if the sum of parameters comprise a cube power
+* 
+*  inputs: 
+*   a = number that is part of cube power
+*   b = number that is part of cube power
+*   c = number that is part of cube power
+*   d = number that is part of cube power
+*   e = number that is part of cube power
+*
+*  output: Returns 1 if sum of parameters are a cube power; 0 otherwise
+* 
+*/
+int is_cubepow(int a, int b, int c, int d, int e) {
 
 	int sum = a + b + c + d + e;
 	int i;
@@ -48,7 +60,19 @@ int is_cubert(int a, int b, int c, int d, int e) {
 
 }
 
-int is_perfect_sqrt(int a, int b, int c) {
+/*
+* is_perfect_sqr() returns if the sum of parameters comprise a cube power
+* 
+*  inputs: 
+*   a = number that is part of perfect square
+*   b = number that is part of perfect square
+*   c = number that is part of perfect square
+*
+*  output: Returns 1 if sum of parameters are a perfect square; 0 otherwise
+* 
+*/
+
+int is_perfect_sqr(int a, int b, int c) {
 	int sum = a + b + c;
 	int i;
 
